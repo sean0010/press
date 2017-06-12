51ca444889a841d9ba9f397fc1cb3c94c33f752d
sean0010 <waysummer@gmail.com>
2017-06-13 00:32:43 +0700

    convert video url to iframe tag

--- steem/js/steem.plugin.js
+++ steem/js/steem.plugin.js
@@ -470,6 +470,29 @@ var remarkable = new Remarkable({
 	quotes: '“”‘’'
 });
 
+
+function convertMedia(html) {
+	var pattern1 = /(?:http?s?:\/\/)?(?:www\.)?(?:vimeo\.com)\/?(.+)/g;
+	var pattern2 = /(?:http?s?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g;
+	var pattern3 = /([-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?(?:jpg|jpeg|gif|png))/gi;
+
+	if (pattern1.test(html)) {
+		var replacement = '<div style="position:relative;height:0;padding-bottom:56.2%"><iframe width="640" height="360" src="//player.vimeo.com/video/$1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
+		var html = html.replace(pattern1, replacement);
+	}
+
+	if (pattern2.test(html)) {
+		var replacement = '<div style="position:relative;height:0;padding-bottom:56.2%"><iframe width="640" height="360" src="http://www.youtube.com/embed/$1?rel=0?ecver=2" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>';
+		var html = html.replace(pattern2, replacement);
+	} 
+
+	if (pattern3.test(html)) {
+		var replacement = '<a href="$1" target="_blank"><img class="sml" src="$1" /></a><br />';
+		var html = html.replace(pattern3, replacement);
+	} 
+	return html;
+}
+
 function markdown2html(markdown) {
 	var jsonMetadata = {};
 	jsonMetadata.image = jsonMetadata.image || [];
@@ -483,10 +506,7 @@ function markdown2html(markdown) {
 	});
 
 	markdown = linkify(markdown);
-
-	//if (_.has(embeds, '[0].embed')) {
-	//	embeds.forEach((embed) => { markdown = markdown.replace(embed.url, embed.embed); });
-	//}
+	markdown = convertMedia(markdown);
 	markdown = remarkable.render(markdown);
 
 	if (_.has(jsonMetadata, 'image[0]')) {
